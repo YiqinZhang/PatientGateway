a3f74d7c797916951cc2d0916c28c305f269a225
Yiqin <zhangyiqin217@gmail.com>
2022-03-16 23:44:35 -0400

    add test

--- test/test_app.py
+++ test/test_app.py
@@ -1,9 +1,18 @@
+import requests
 from flask import request
 from app import app
 import pytest
 
-class TestRequest():
+
+BASE = "http://127.0.0.1:5000/"
+
+
+class TestRequest:
     def test_request_input(self):
         with app.test_request_context('/login', method='POST'):
             assert request.path == '/login'
             assert request.method == 'POST'
+
+    def test_login_response(self):
+        response = requests.get(BASE + "login")
+        assert response.status_code == 200
