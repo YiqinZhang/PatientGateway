e56ac3c247c7372c47cf4f23bdface51caef4da7
EveZhang <55336328+YiqinZhang@users.noreply.github.com>
2022-03-17 14:50:22 -0400

    Update test_user.py

--- test/test_user.py
+++ test/test_user.py
@@ -1,7 +1,7 @@
 import pytest
 import requests
-from user import *
-#
+import user 
+
 BASE = "http://127.0.0.1:5000/"
 
 user_data = [{"name": "tim", "DoB": "07/19/1998", "gender": "male", "bloodtype": "A", "height": 180, "weight": 170},
@@ -12,15 +12,15 @@ user_data = [{"name": "tim", "DoB": "07/19/1998", "gender": "male", "bloodtype":
 class TestUser:
     def test_abort_if_user_id_doesnt_exist(self):
         with pytest.raises(KeyError):
-            abort_if_user_id_doesnt_exist(10000)
+            user.abort_if_user_id_doesnt_exist(10000)
 
     def test_abort_empty_user_id(self):
         with pytest.raises(TypeError):
-            abort_if_user_id_doesnt_exist()
+            user.abort_if_user_id_doesnt_exist()
 
     def test_abort_if_id_exists(self):
         with pytest.raises(KeyError):
-            abort_if_id_exists(1)
+            user.abort_if_id_exists(1)
 
     def test_login(self):
         response = requests.get(BASE + "user/1")
