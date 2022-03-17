f9efc078f448054acae370a5db067d7e3d1588e5
Yiqin <zhangyiqin217@gmail.com>
2022-03-16 23:22:51 -0400

    modify test user

--- test/test_user.py
+++ test/test_user.py
@@ -1,20 +1,30 @@
-import unittest
+import pytest
 import requests
+from user import *
 
 BASE = "http://127.0.0.1:5000/"
 
-data = [{"name": "tim", "DoB": "07/19/1998", "gender": "male", "bloodtype":"A", "height": 180, "weight": 170},
-        {"name": "eve", "DoB": "12/24/2004", "gender": "female", "bloodtype":"B", "height": 166, "weight": 120},
-        {"name": "adam", "DoB": "02/22/2002", "gender": "female", "bloodtype":"AB", "height": 178, "weight": 160}]
+user_data = [{"name": "tim", "DoB": "07/19/1998", "gender": "male", "bloodtype": "A", "height": 180, "weight": 170},
+             {"name": "eve", "DoB": "12/24/2004", "gender": "female", "bloodtype": "B", "height": 166, "weight": 120},
+             {"name": "adam", "DoB": "02/22/2002", "gender": "female", "bloodtype": "AB", "height": 178, "weight": 160}]
 
-for i in range(len(data)):
-    response = requests.put(BASE + "user/" + str(i), data[i])
-    print(response.json())
 
-response = requests.delete(BASE + "user/0")
-print(response)
-input()
+class TestUser:
+    def test_abort_if_user_id_doesnt_exist(self):
+        with pytest.raises(KeyError):
+            abort_if_user_id_doesnt_exist(10000)
 
-response = requests.get(BASE + "user/2")
-print(response.json())
-input()
+    def test_abort_empty_user_id(self):
+        with pytest.raises(TypeError):
+            abort_if_user_id_doesnt_exist()
+
+    def test_abort_if_id_exists(self):
+        with pytest.raises(KeyError):
+            abort_if_id_exists(1)
+
+    def test_get(self):
+        response = requests.get(BASE + "user/1")
+        result = response.json()
+        assert response.status_code == 200
+        assert result["user_id"] == 2
+
