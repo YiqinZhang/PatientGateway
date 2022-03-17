dcdb06b25888e469fc52f7f69f34b6aabc9a3983
Yiqin <zhangyiqin217@gmail.com>
2022-03-17 15:32:17 -0400

    modify test user

--- test_user.py
+++ test_user.py
@@ -3,11 +3,11 @@ import requests
 import user 
 
 
-BASE = "http://127.0.0.1:5000/"
+# BASE = "http://127.0.0.1:5000/"
 
-user_data = [{"name": "tim", "DoB": "07/19/1998", "gender": "male", "bloodtype": "A", "height": 180, "weight": 170},
-             {"name": "eve", "DoB": "12/24/2004", "gender": "female", "bloodtype": "B", "height": 166, "weight": 120},
-             {"name": "adam", "DoB": "02/22/2002", "gender": "female", "bloodtype": "AB", "height": 178, "weight": 160}]
+# user_data = [{"name": "tim", "DoB": "07/19/1998", "gender": "male", "bloodtype": "A", "height": 180, "weight": 170},
+#              {"name": "eve", "DoB": "12/24/2004", "gender": "female", "bloodtype": "B", "height": 166, "weight": 120},
+#              {"name": "adam", "DoB": "02/22/2002", "gender": "female", "bloodtype": "AB", "height": 178, "weight": 160}]
 
 
 class TestUser:
@@ -23,9 +23,9 @@ class TestUser:
         with pytest.raises(KeyError):
             user.abort_if_id_exists(1)
 
-    def test_login(self):
-        response = requests.get(BASE + "user/1")
-        result = response.json()
-        assert response.status_code == 200
-        assert result["user_id"] == 2
+    # def test_login(self):
+    #     response = requests.get(BASE + "user/1")
+    #     result = response.json()
+    #     assert response.status_code == 200
+    #     assert result["user_id"] == 2
 
