ca567794b7bd8fe98562ebb448e8645539997bf3
Yiqin <zhangyiqin217@gmail.com>
2022-05-06 00:49:15 -0400

    modify chat

--- Modules/chat.py
+++ Modules/chat.py
@@ -1,9 +1,9 @@
 # from typing import List, Union
 import json
-import Modules.user as user
+import user
 from datetime import datetime
 
-with open('./Modules/inbox.json', 'r') as f:
+with open('inbox.json', 'r') as f:
     chat_dict = json.load(f)
     if not chat_dict:
         chat_dict = {}
@@ -52,15 +52,14 @@ def send_chat(chat):
 
 # send_chat(Chat(1, 2, "The First message"))
 # send_chat(Chat(2, 3, "Second message sent"))
-def get_chat_history(user):
-    user.abort_if_user_id_doesnt_exist(user)
+def get_chat_history(user_id):
+    user.abort_if_user_id_doesnt_exist(user_id)
     history = {}
     for k, v in chat_dict.items():
-        if user == v["sender"] or user == v["to"]:
+        if user_id == v["sender"] or user_id == v["to"]:
             # history.update({k, v})
             history[k] = v
     return history
-
 
 
 # print(get_chat_history(1))
@@ -78,61 +77,3 @@ def get_chat_history(user):
 #         user_dict.update({"users": data})
 #         json.dump(user_dict, f, indent=2)
 #     return deleted
-
-
-def create_appointment_table(db):
-    cursor = db.cursor()
-
-    sql = 'drop table if exists appointment'
-    cursor.execute(sql)
-
-    sql = 'create table appointment (a_id INT AUTO_INCREMENT, doctor_id INT NOT NULL, patient_id INT NOT NULL,' \
-          'appointment_date DATETIME, start TIME, finish TIME, PRIMARY KEY (a_id)) '
-    cursor.execute(sql)
-    db.commit()
-    db.close()
-
-
-def store_appointment(db, doctor, patient, date, startime, endtime):
-    cursor = db.cursor()
-    sql = 'insert into appointment (doctor_id, patient_id, appointment_date, start, finish) values (%s, %s, %s, %s, %s)'
-    cursor.execute(sql, (doctor, patient, date, startime, endtime))
-    db.commit()
-    db.close()
-    return cursor.lastrowid
-
-
-def get_appointment(db, username):
-    cursor = db.cursor()
-    sql = 'select * from appointment where doctor_id = ? or patient_id = ?'
-    cursor.execute(sql, (username, username))
-    results = cursor.fetchall()
-    db.close()
-    return results
-
-
-def make_appointment(db, doctor, patient, date, startime, endtime):
-    # db = pymysql.connect(host="localhost", user="root", password=password, database="medical_platform")
-    cursor = db.cursor()
-    sql = "select * from appointment where doctor_id = %s or patient_id = %s"
-    cursor.execute(sql, (doctor, patient))
-    results = cursor.fetchall()
-    # check available
-    for entry in results:
-        if (datetime.strptime(date, '%Y-%m-%d') - entry[3]).days == 0:
-            if format_time(str(entry[4])) <= format_time(startime) < format_time(str(entry[5])):
-                print("Time conflict.")
-                return
-    store_appointment(doctor, patient, date, startime, endtime, password=password)
-    print('Appointment are reserved')
-    db.close()
-    return cursor.lastrowid
-
-# create_appointment_table(password)
-# doctor = 3
-# patient = 4
-# date = "2022-6-1"
-# start = '10:00:00'
-# end = '12:00:00'
-# # store_appointment(doctor, patient, date, start, end, password)
-# make_appointment(doctor, patient, date, start, end, password)
